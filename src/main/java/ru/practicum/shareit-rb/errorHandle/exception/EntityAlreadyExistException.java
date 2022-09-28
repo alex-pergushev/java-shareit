de7abfb9abb10050c9612b00883f388906612cfd
Alex Pergushev <alexander.pergushev@yandex.ru>
2022-09-28 17:24:49 +0700

    feat: Sprint 14

--- src/main/java/ru/practicum/shareit-rb/errorHandle/exception/EntityAlreadyExistException.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.shareit.errorHandle.exception;
-
-public class EntityAlreadyExistException extends RuntimeException {
-    public EntityAlreadyExistException(String message) {
-        super(message);
-    }
-}
--- src/main/java/ru/practicum/shareit-rb/errorHandle/exception/EntityNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.shareit.errorHandle.exception;
-
-public class EntityNotFoundException extends RuntimeException{
-    public EntityNotFoundException(String message) {
-        super(message);
-    }
-}
